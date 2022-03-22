5d3886ca2f6b4ca71285c71a53bf3fd4534f26e8
Anatoliy Yanuzov <anatolii.qnuzov@gmail.com>
2022-03-22 19:50:44 +0200

    added solution

---
+++ test/04.sum-of-numbers.test.js
@@ -0,0 +1,36 @@
+const assert = require('chai').assert;
+const sum = require('../04.sum-of-numbers');
+
+describe('Test sum functionality', () => {
+
+    it('Should add positive numbers', () => {
+        // Arrange
+        let input = [1, 2, 3, 4, 5];
+
+        let expectedResult = 15;
+        //Act
+        let actualResult = sum(input);
+        //Assert
+        assert.strictEqual(actualResult, expectedResult);
+    });
+
+    it('Should return false when adding positive numbers', () => {
+        // Arrange
+        let input = [10, 20, 30, 40];
+        let expectedResult = 15;
+        //Act
+        let actualResult = sum(input);
+        //Assert
+        assert.notEqual(actualResult, expectedResult);
+    });
+
+    it('Should pass when adding negative numbers', () => {
+        // Arrange
+        let input = [-1, -2, -3, -4];
+        let expectedResult = -10;
+        //Act
+        let actualResult = sum(input);
+        //Assert
+        assert.equal(actualResult, expectedResult);
+    });
+});
